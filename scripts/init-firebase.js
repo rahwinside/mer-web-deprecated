26fd75a4873a1f6ed2e29ffee5372168a04d379b
Rahul Pon <theflyingrahul@gmail.com>
2020-04-05 18:10:45 +0530

    Firebase Initialised

---
+++ scripts/init-firebase.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var firebaseConfig = {
+	apiKey: "AIzaSyCOrjgC16B1E82qtckxBvjWl30PjIWYQjo",
+	authDomain: "mercommunity-arch.firebaseapp.com",
+	databaseURL: "https://mercommunity-arch.firebaseio.com",
+	projectId: "mercommunity-arch",
+	storageBucket: "mercommunity-arch.appspot.com",
+	messagingSenderId: "506464565094",
+	appId: "1:506464565094:web:ac04d71ff4fad71879faa7",
+	measurementId: "G-RSCEML1HER"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
